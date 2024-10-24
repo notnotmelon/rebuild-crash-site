7c6fd46222a7f2afcd369fd0e85f21ea9eec0536
notnotmelon <zacharypicco@gmail.com>
2024-10-24 16:27:07 -0500

    feat: Updated for 2.0

--- control.lua
+++ control.lua
@@ -1,6 +1,13 @@
 script.on_event(defines.events.on_entity_died, function(event)
 	local entity = event.entity
-	entity.surface.spill_item_stack(entity.position, {name = 'crash-site-spaceship', count = 1}, true, 'player')
+	entity.surface.spill_item_stack{
+		position = entity.position,
+		stack = {name = 'crash-site-spaceship', count = 1},
+		enable_looted = true,
+		force = entity.force_index,
+		allow_belts = true,
+		use_start_position_on_failure = true
+	}
 end, {
 	{filter = 'name', name = 'crash-site-spaceship'},
 	{filter = 'type', type = 'container', mode = 'and'}
